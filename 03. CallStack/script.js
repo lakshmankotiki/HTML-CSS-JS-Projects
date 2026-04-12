c3427e799074ee17bb2b1c223f9053acde5255fd
Lakshman <kotiki.lakshman1996@gmail.com>
2026-04-12 15:43:02 +0530

    another example for scope chain

--- 03. CallStack/script.js
+++ 03. CallStack/script.js
@@ -1,18 +1,38 @@
+// /**
+//  * Example 1----
+//  */
+// getName1(); // gives error because Js treats this as a variable which is already stored as a undefined, undefined() gives error
+
+// getName(); // it prints the actual output because during the memory creation phase it stores as actual function definition
+
+// console.log(x); // undefined
+
+// console.log(getName); // prints the function definition
+// var x = 7;
+// function getName() {
+//     console.log("getName function: Lakshman");
+// }
+
+// var getName1 = function() {
+//     console.log("getName1 function: Chandu");
+// }
+
+
+
 /**
- * Example 1----
+ * Example 2
  */
-getName1(); // gives error because Js treats this as a variable which is already stored as a undefined, undefined() gives error
+var x = 10;
+a();
+b();
+console.log(x);
 
-getName(); // it prints the actual output because during the memory creation phase it stores as actual function definition
-
-console.log(x); // undefined
-
-console.log(getName); // prints the function definition
-var x = 7;
-function getName() {
-    console.log("getName function: Lakshman");
+function a() {
+    var x = 20;
+    console.log(x);
 }
 
-var getName1 = function() {
-    console.log("getName1 function: Chandu");
+function b() {
+    var x = 30;
+    console.log(x);
 }
