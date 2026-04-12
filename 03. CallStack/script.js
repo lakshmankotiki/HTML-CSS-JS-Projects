357f6c969615b39f5f2cedab356d2fe498e00417
Lakshman <kotiki.lakshman1996@gmail.com>
2026-04-12 15:04:15 +0530

    callstack functionality

---
+++ 03. CallStack/script.js
@@ -0,0 +1,18 @@
+/**
+ * Example 1----
+ */
+getName1(); // gives error because Js treats this as a variable which is already stored as a undefined, undefined() gives error
+
+getName(); // it prints the actual output because during the memory creation phase it stores as actual function definition
+
+console.log(x); // undefined
+
+console.log(getName); // prints the function definition
+var x = 7;
+function getName() {
+    console.log("getName function: Lakshman");
+}
+
+var getName1 = function() {
+    console.log("getName1 function: Chandu");
+}
